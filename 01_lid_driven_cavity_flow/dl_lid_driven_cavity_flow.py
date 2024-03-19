f9f2b6665186cca3556ba7913424cfda5fd3b0d3
jjderlatka <jjd61@cam.ac.uk>
2024-03-19 11:46:54 +0000

    major code refactor

--- 01_lid_driven_cavity_flow/dl_lid_driven_cavity_flow.py
+++ 01_lid_driven_cavity_flow/dl_lid_driven_cavity_flow.py
@@ -118,15 +118,60 @@ def generate_parameters_list(parameteres_values):
     return np.array([Parameters(*vals) for vals in parameteres_values])
 
 
-def generate_rb_solutions_list(parameters_set):
-    my_parameters_set = np.array_split(parameters_set, MPI.COMM_WORLD.Get_size())[MPI.COMM_WORLD.Get_rank()]
+def generate_solutions_list(global_training_set):
+    snapshots_u_matrix = rbnicsx.backends.FunctionsList(problem_parametric._V)
+    snapshots_p_matrix = rbnicsx.backends.FunctionsList(problem_parametric._Q)
+
+    # TODO discuss: what are the benefits or choosing every 10th vs continous sections of 10. Any caching benefit? Any way to use the similarity between solving similar solutions? Maybe iterative methods could use the solution to neighbouring parameter as the starting point?
+    # TODO not necessarily helpful atm, as LU is used (am I right?) But maybe could switch to a different solver, slower for one solve, but faster for a range of them
+    my_training_set = np.array_split(global_training_set, MPI.COMM_WORLD.Get_size())[MPI.COMM_WORLD.Get_rank()]
+    
+    for (params_index, params) in enumerate(my_training_set):
+        print(rbnicsx.io.TextLine(f"{params_index+1} of {my_training_set.shape[0]}", fill="#"))
+        print("High fidelity solve for params =", params)
+        snapshot_u, snapshot_p = problem_parametric.solve(params)
+
+        snapshots_u_matrix.append(snapshot_u)
+        snapshots_p_matrix.append(snapshot_p)
+
+    mpi_print(f"Rank {MPI.COMM_WORLD.Get_rank()} has {np.shape(snapshots_p_matrix)} snapshots.")
+    
+    snapshots_u_matrix = gather_functions_list(snapshots_u_matrix, 0)
+    snapshots_p_matrix = gather_functions_list(snapshots_p_matrix, 0)
+
+    return snapshots_u_matrix, snapshots_p_matrix
+
+
+def proper_orthogonal_decomposition(reduced_problem, snapshots_matrix):
+    # NOTE POD parallelization: correlation matrix assembly could be paralalleized
+    # NOTE SLEPc eigensolve underlying POD could also be parallalized
+    
+    Nmax = 100 # the number of basis functions
+
+    if MPI.COMM_WORLD.Get_rank() == 0:
+        eigenvalues, modes, _ = \
+            rbnicsx.backends.\
+            proper_orthogonal_decomposition(snapshots_matrix,
+                                            reduced_problem._inner_product_action,
+                                            N=Nmax, tol=1.e-6)
+        
+    else:
+        eigenvalues, modes = None, None
+
+    eigenvalues, modes = MPI.COMM_WORLD.bcast(eigenvalues, root=0), bcast_functions_list(modes, reduced_problem._function_space, 0)
+
+    reduced_problem.set_reduced_basis(modes)
+
+
+def generate_rb_solutions_list(global_parameters_set):
+    my_parameters_set = np.array_split(global_parameters_set, MPI.COMM_WORLD.Get_size())[MPI.COMM_WORLD.Get_rank()]
 
     rb_snapshots_u_matrix = np.empty((len(my_parameters_set), reduced_problem_u.rb_dimension()))
     rb_snapshots_p_matrix = np.empty((len(my_parameters_set), reduced_problem_p.rb_dimension()))
 
     for (params_index, params) in enumerate(my_parameters_set):
         print(rbnicsx.io.TextLine(f"{params_index+1} of {len(my_parameters_set)}", fill="#"))
-        print("high fidelity solve for params =", params)
+        print("High fidelity solve for params =", params)
         snapshot_u, snapshot_p = problem_parametric.solve(params)
 
         rb_snapshot_u = reduced_problem_u.project_snapshot(snapshot_u)
@@ -134,8 +179,6 @@ def generate_rb_solutions_list(parameters_set):
 
         rb_snapshots_u_matrix[params_index, :] = rb_snapshot_u
         rb_snapshots_p_matrix[params_index, :] = rb_snapshot_p
-
-    print("")
 
     rb_snapshots_u_matrix = MPI.COMM_WORLD.gather(rb_snapshots_u_matrix, 0)
     rb_snapshots_p_matrix = MPI.COMM_WORLD.gather(rb_snapshots_p_matrix, 0)
@@ -150,26 +193,21 @@ def generate_rb_solutions_list(parameters_set):
     return rb_snapshots_u_matrix, rb_snapshots_p_matrix
 
 
-def prepare_test_and_training_sets(samples):
-    paramteres_list_ = generate_parameters_list(generate_parameters_values_list(samples))
-    
-    np.random.shuffle(paramteres_list_)
-    solutions_list_u, solutions_list_p = generate_rb_solutions_list(paramteres_list_)
+def prepare_test_and_training_sets(parameters_list, solutions_u, solutions_p):
+    num_training_samples = int(0.7 * len(parameters_list))
+    num_validation_samples = len(parameters_list) - num_training_samples
 
-    num_training_samples = int(0.7 * len(paramteres_list_))
-    num_validation_samples = len(paramteres_list_) - num_training_samples
+    paramteres_values_list = np.array([p.to_numpy() for p in parameters_list])
 
-    paramteres_list = np.array([p.to_numpy() for p in paramteres_list_])
-
-    input_training_set = paramteres_list[:num_training_samples, :]
-    solutions_u_training_set = solutions_list_u[:num_training_samples, :]
-    solutions_p_training_set = solutions_list_p[:num_training_samples, :]
+    input_training_set = paramteres_values_list[:num_training_samples, :]
+    solutions_u_training_set = solutions_u[:num_training_samples, :]
+    solutions_p_training_set = solutions_p[:num_training_samples, :]
     training_dataset_u = CustomDataset(input_training_set, solutions_u_training_set)
     training_dataset_p = CustomDataset(input_training_set, solutions_p_training_set)
 
-    input_validation_set = paramteres_list[num_training_samples:, :]
-    solutions_u_validation_set = solutions_list_u[num_training_samples:, :]
-    solutions_p_validation_set = solutions_list_p[num_training_samples:, :]
+    input_validation_set = paramteres_values_list[num_training_samples:, :]
+    solutions_u_validation_set = solutions_u[num_training_samples:, :]
+    solutions_p_validation_set = solutions_p[num_training_samples:, :]
     validation_dataset_u = CustomDataset(input_validation_set, solutions_u_validation_set)
     validation_dataset_p = CustomDataset(input_validation_set, solutions_p_validation_set)
 
@@ -200,8 +238,8 @@ def train_NN(training_dataset, validation_dataset):
     # u only to begin with
     
     device = "cpu"
-
     batch_size = 64
+
     train_dataloader = DataLoader(training_dataset, batch_size=batch_size)
     test_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
 
@@ -210,9 +248,7 @@ def train_NN(training_dataset, validation_dataset):
     output_size = len(training_dataset[0][1])
     print(f"NN {input_size=}, {output_size=}")
 
-    model = HiddenLayersNet(input_size, [512, 512], output_size, Tanh()).to(device)
-    # TODO remove 
-    model.double() # Convert the entire model to Double (or would have to convert input and outputs to floats (they're now doubles))
+    model.double() # TODO remove? Convert the entire model to Double (or would have to convert input and outputs to floats (they're now doubles))
     print(model)
 
     # TODO investigate the loss function
@@ -220,7 +256,8 @@ def train_NN(training_dataset, validation_dataset):
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
     # TODO plot the evolution with number of epochs
-    epochs = 10
+    # (plot the loss function evolution, but also prediction made with NN trained up to given epoch)
+    epochs = 100
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         # TODO make a pull request removing reduced_problem argument from these functions
@@ -231,126 +268,81 @@ def train_NN(training_dataset, validation_dataset):
     return model
 
 
+def save_preview(preview_parameter, model, problem_parametric, reduced_problem_u, reduced_problem_p):
+    # Infer solution
+    X = torch.tensor(preview_parameter.to_numpy())
+    rb_pred = model(X)
+    # TODO: ask why it didn't work without specyfing the communicator? How did it know
+    rb_pred_vec = PETSc.Vec().createWithArray(rb_pred.detach().numpy(), comm=MPI.COMM_SELF)
+
+    # Full solution
+    fem_u, fem_p = problem_parametric.solve(preview_parameter)
+    problem_parametric.save_results(preview_parameter, problem_parametric.interpolated_velocity(fem_u), fem_p, name_suffix="_exact")
+
+    # POD solution
+    rb_snapshot_u = reduced_problem_u.project_snapshot(fem_u)
+    rb_snapshot_p = reduced_problem_p.project_snapshot(fem_p)
+    reconstructed_u = reduced_problem_u.reconstruct_solution(rb_snapshot_u)
+    reconstructed_p = reduced_problem_p.reconstruct_solution(rb_snapshot_p)
+    problem_parametric.save_results(preview_parameter, problem_parametric.interpolated_velocity(reconstructed_u), reconstructed_p, name_suffix="_rb")
+    # problem_parametric.save_results(p, problem_parametric.interpolated_velocity(fem_u), fem_p, name_suffix="_rb")
+
+    # NN solution
+    # TODO make a pull request adding the option to supply solution to error analysis function
+    full_order_pred = reduced_problem_u.reconstruct_solution(rb_pred_vec)
+    interpolated_pred = problem_parametric.interpolated_velocity(full_order_pred)
+    problem_parametric.save_results(p, interpolated_pred, name_suffix="_pred")
+
+
 if __name__ == "__main__":
     timer = Timer()
-    
     # TODO probably remove
     np.random.seed(0)
 
+    # Load mesh
     mesh, cell_tags, facet_tags = dolfinx.io.gmshio.read_from_msh("mesh.msh", MPI.COMM_SELF, 0, gdim=2)
-    
-    timer.timestamp("Mesh loaded")
-
     mpi_print(f"Number of local cells: {mesh.topology.index_map(2).size_local}")
     mpi_print(f"Number of global cells: {mesh.topology.index_map(2).size_global}")
+    timer.timestamp("Mesh loaded") # TODO is it a problem that I'm timing i/o operations?
 
+
+    # Set up the problem
     problem_parametric = ProblemOnDeformedDomain(mesh, cell_tags, facet_tags, HarmonicMeshMotion)
-
-    # POD
-
-    # TODO ask: why waste time broadcasting this set to every rank, if each can generate it itself in very few operations
-    global_training_set = generate_parameters_list(generate_parameters_values_list([6, 6, 6]))
-
-    Nmax = 100 # the number of basis functions
-
-    print(rbnicsx.io.TextBox("POD offline phase begins", fill="="))
-
-    # TODO All below could be enclosed in a loop
-    print("set up snapshots matrix")
-    snapshots_u_matrix = rbnicsx.backends.FunctionsList(problem_parametric._V)
-    snapshots_p_matrix = rbnicsx.backends.FunctionsList(problem_parametric._Q)
-
-    # TODO discuss: what are the benefits or choosing every 10th vs continous sections of 10. Any caching benefit? Any way to use the similarity between solving similar solutions? Maybe iterative methods could use the solution to neighbouring parameter as the starting point?
-    # TODO not necessarily helpful atm, as LU is used (am I right?) But maybe could switch to a different solver, slower for one solve, but faster for a range of them
-
-    # TODO ask: why do dolfinx and rbnicsx have the two level interface? Each function calls its backend version
-    my_training_set = np.array_split(global_training_set, MPI.COMM_WORLD.Get_size())[MPI.COMM_WORLD.Get_rank()]
-    # mpi_print(my_training_set)
     
-    for (params_index, params) in enumerate(my_training_set):
-        print(rbnicsx.io.TextLine(f"{params_index+1} of {my_training_set.shape[0]}", fill="#"))
-        print("high fidelity solve for params =", params)
-        snapshot_u, snapshot_p = problem_parametric.solve(params)
-
-        snapshots_u_matrix.append(snapshot_u)
-        snapshots_p_matrix.append(snapshot_p)
-
-    mpi_print(f"Rank {MPI.COMM_WORLD.Get_rank()} has {np.shape(snapshots_p_matrix)} snapshots.")
-
-    snapshots_u_matrix = gather_functions_list(snapshots_u_matrix, 0)
-    snapshots_p_matrix = gather_functions_list(snapshots_p_matrix, 0)
-
-    timer.timestamp("POD training set calculated")
-
-    mpi_print(f"Matrix built on rank {MPI.COMM_WORLD.Get_rank()} has {np.shape(snapshots_u_matrix)} snapshots.")
-    mpi_print(f"Matrix built on rank {MPI.COMM_WORLD.Get_rank()} has {np.shape(snapshots_p_matrix)} snapshots.")
-
-    print("set up reduced problem")
     reduced_problem_u = PODANNReducedProblem(problem_parametric, problem_parametric._V)
     reduced_problem_p = PODANNReducedProblem(problem_parametric, problem_parametric._Q)
 
-    # NOTE POD parallelization
-    # correlation matrix assembly could be paralalleized
+    # POD
+    print(rbnicsx.io.TextBox("POD offline phase begins", fill="="))
 
-    if MPI.COMM_WORLD.Get_rank() == 0:
-        print(rbnicsx.io.TextLine("perform POD", fill="#"))
-        eigenvalues_u, modes_u, _ = \
-            rbnicsx.backends.\
-            proper_orthogonal_decomposition(snapshots_u_matrix,
-                                            reduced_problem_u._inner_product_action,
-                                            N=Nmax, tol=1.e-6)
+    # TODO ask: why waste time broadcasting this set to every rank, if each can generate it itself in very few operations
+    global_training_set = generate_parameters_list(generate_parameters_values_list([7, 7, 7]))
+    snapshots_u_matrix, snapshots_p_matrix = generate_solutions_list(global_training_set)
+    mpi_print(f"Matrix built on rank {MPI.COMM_WORLD.Get_rank()} has {np.shape(snapshots_p_matrix)} snapshots.")
+    timer.timestamp("POD training set calculated")
 
-        eigenvalues_p, modes_p, _ = \
-            rbnicsx.backends.\
-            proper_orthogonal_decomposition(snapshots_p_matrix,
-                                            reduced_problem_p._inner_product_action,
-                                            N=Nmax, tol=1.e-6)
-        
-    else:
-        eigenvalues_u, modes_u = None, None
-        eigenvalues_p, modes_p = None, None
-
-    eigenvalues_u, modes_u = MPI.COMM_WORLD.bcast(eigenvalues_u, root=0), bcast_functions_list(modes_u, problem_parametric._V, 0)
-    eigenvalues_p, modes_p = MPI.COMM_WORLD.bcast(eigenvalues_p, root=0), bcast_functions_list(modes_p, problem_parametric._Q, 0)
-
-    reduced_problem_u.set_reduced_basis(modes_u)
-    reduced_problem_p.set_reduced_basis(modes_p)
-
+    print(rbnicsx.io.TextLine("perform POD", fill="#"))
+    proper_orthogonal_decomposition(reduced_problem_u, snapshots_u_matrix)
+    proper_orthogonal_decomposition(reduced_problem_p, snapshots_p_matrix)
     timer.timestamp("Reduced basis calculated")
 
+    # NN
     # Generate training data
-    training_dataset_u, training_dataset_p, validation_dataset_u, validation_dataset_p = prepare_test_and_training_sets([6, 6, 6])
-
+    paramteres_list = generate_parameters_list(generate_parameters_values_list([7, 7, 7]))
+    np.random.shuffle(paramteres_list)
+    solutions_list_u, solutions_list_p = generate_rb_solutions_list(paramteres_list)
+    training_dataset_u, training_dataset_p, validation_dataset_u, validation_dataset_p =\
+        prepare_test_and_training_sets(paramteres_list, solutions_list_u, solutions_list_p)
     timer.timestamp("NN dataset calculated")
 
     if MPI.COMM_WORLD.Get_rank() == 0:
 
         model = train_NN(training_dataset_u, validation_dataset_u)
-
         timer.timestamp("NN trained")
 
-        # Infer solution and save it
-        p = Parameters(1, 2)
-        X = torch.tensor(p.to_numpy())
-        rb_pred = model(X)
-        # TODO: ask why it didn't work without specyfing the communicator? How did it know
-        rb_pred_vec = PETSc.Vec().createWithArray(rb_pred.detach().numpy(), comm=MPI.COMM_SELF)
-
-        # Full solution
-        print("Solve")
-        fem_u, fem_p = problem_parametric.solve(p)
-        print("Save results with class method")
-        problem_parametric.save_results(p, problem_parametric.interpolated_velocity(fem_u), fem_p)
-
-        print(f"Projecting the prediction to full basis")
-        # TODO make a pull request adding the option to supply solution to error analysis function
-        full_order_pred = reduced_problem_u.reconstruct_solution(rb_pred_vec)
-        interpolated_pred = problem_parametric.interpolated_velocity(full_order_pred)
-        problem_parametric.save_results(p, interpolated_pred, name_suffix="_int_out")
-
-        diff = fem_u - full_order_pred
-        problem_parametric.save_results(p, problem_parametric.interpolated_velocity(diff), name_suffix="_diff")
-
         timer.timestamp("Error analysis completed")
+
+        p = Parameters(1, 2)
+        save_preview(p, model, problem_parametric, reduced_problem_u, reduced_problem_p)
     
     save_all_timestamps(timer, 0)
